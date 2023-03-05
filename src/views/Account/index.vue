9bfef2512aea9fbbb33026a20502d9d15e753a43
绪运 莫 <2265364722@qq.com>
2023-03-05 22:09:16 +0800

    修改基础页面

---
+++ src/views/Account/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    account
+  </div>
+
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Account"
+}
+</script>
+
+<style scoped>
+
+</style>
